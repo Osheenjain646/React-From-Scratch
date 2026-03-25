c4e80dc8ab4a2b25ace8b15ed8543305a386d51c
Osheenjain646 <osheenjain646@gmail.com>
2026-03-26 01:18:11 +0530

    learning the Basics

---
+++ Day-1/react-demos/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h2>Main Content</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, optio? Ratione, labore inventore architecto dolorum excepturi consectetur earum, amet eligendi repellat, quia accusantium sed alias deleniti iste laboriosam sunt expedita!</p>
+    </main>
+  )
+}
+
+export default MainContent
---
+++ Day-1/react-demos/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h1 className='welcomeMessage-h1-class'>Hello , World!</h1>
+      <p>Welcome to learning JSX!</p>
+    </div>
+  )
+}
+
+export default WelcomeMessage
