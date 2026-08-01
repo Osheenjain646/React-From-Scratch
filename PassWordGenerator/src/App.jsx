9ff38c8b21432746d4e44c95df1a93c88a0749b3
Osheenjain646 <osheenjain646@gmail.com>
2026-08-02 00:42:33 +0530

    updated

--- PassWordGenerator/src/App.jsx
+++ PassWordGenerator/src/App.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  // will making the states for password to set it in future
+
+  const [Password , setPassword] = useState("");
+
+  // a state for it's length
+
+  const [Length , setLength] = useState(8); 
+
+  // a state for it's Number is allowed or not
+
+  const [numberAllowed , setnumberAllowed] = useState(false);
+
+  // a state for a character is allowed or not
+
+  const [specialcharAllowed , setspecialcharAllowed] = useState(false);
+
+  const passwordGenerator = () => {
+    let password = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str += "0123456789";
+    if(specialcharAllowed) str += "!@#$%^&*()_{}[]/:;,.<>?~`";
+
+    for(let i=1; i<=str.length; i++){
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      password += str.charAt(char);
+    }
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg  px-4 py-8 text-orange-500 bg-gray-500'>
+        <h1 className='text-2xl font-bold mb-4 text-center'>Password Generator</h1>
+      </div>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+        type="text"
+        value={Password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password'
+        readOnly
+        />
+      </div>
+    </>
   )
 }
 
--- PassWordGenerator/src/notes.md
+++ PassWordGenerator/src/notes.md
@@ -4,8 +4,8 @@ const cachedFn = useCallback(fn, dependencies)
 
 React Compiler automatically memoizes values and functions, reducing the need for manual useCallback calls. You can use the compiler to handle memoization automatically.
 
-Reference 
-useCallback(fn, dependencies) 
+Reference
+useCallback(fn, dependencies)
 Call useCallback at the top level of your component to cache a function definition between re-renders:
 
 import { useCallback } from 'react';
@@ -17,5 +17,4 @@ export default function ProductPage({ productId, referrer, theme }) {
       orderDetails,
     });
   }, [productId, referrer]);
-
-
+}
